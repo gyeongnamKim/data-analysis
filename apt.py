1768283a3e9c4c85eedceff62252c6dec5fed668
gyeongnamKim <rudska1885@gmail.com>
2021-07-03 15:37:05 +0900

    no message

---
+++ apt.py
@@ -0,0 +1,60 @@
+import pandas as p
+#소수점 제한두기
+p.options.display.float_format = '{:.2f}'.format
+#파일 가져오기 (encoding="cp949"는 엑셀파일 불러올때 한글이 안깨진다)
+csv_read = p.read_csv('./apt.csv', encoding="cp949")
+#자료 수
+print('자료 수\t\t행 수 :',len(csv_read.index))
+print('\t\t\t열 수 :',len(csv_read.columns))
+
+#자료 파악하기
+print('이 자료는 지역별 아파트 거래의 세부내용을 담은 파일이다.')
+
+#특정 열 (지역) 출력하기
+print('\n\n지역 정보 출력')
+print(csv_read.iloc[:,0])
+
+#면적 130이상 가격 1억5천 미만 아파트 출력하기
+print('\n\n면적 130이상 가격 1억5천 미만 아파트 단지명 출력하기')
+area_csv = csv_read['면적'] >= 130
+area_csv = csv_read[area_csv]
+value_csv = area_csv['가격'] < 13000
+value_csv = area_csv[value_csv]
+print(value_csv['아파트'])
+
+#4억원 이상으로 거래된 아파트 출력하기
+print('\n\n 4억원 이상으로 거래된 아파트 출력하기')
+sort_price = csv_read['가격'] >= 40000
+print(csv_read[sort_price])
+
+#가격 면적 단가 10개 출력하기
+print('\n\n 가격 면적 단가 10개 출력하기')
+csv_read['단가'] = csv_read['가격'] / csv_read['면적']
+print(csv_read[['가격','단가','면적']].head(10))
+
+#가격 오름차순 정렬하기
+print('\n\n 가격 오름차순 정렬하기')
+sort_csv = csv_read.sort_values(by='가격',ascending=True,inplace=False)
+print(sort_csv[['지역', '번지', '본번', '부번', '아파트', '면적', '계약년월', '계약일', '단가', '층', '건축년도',
+       '도로명', '가격']])
+
+#25억 넘는 아파트만 출력하기
+print('\n\n 25억 넘는 아파트만 출력하기')
+price25_csv = csv_read['가격'] >= 250000
+price25_csv = csv_read[price25_csv]
+print(price25_csv[['지역', '번지', '본번', '부번', '아파트', '면적', '계약년월', '계약일', '단가', '층', '건축년도',
+       '도로명', '가격']])
+
+#강릉 아파트만 출력
+print('\n\n 강릉 아파트만 출력')
+gang_csv = csv_read['지역'].str.contains('강릉',na=False)
+print(csv_read[gang_csv])
+
+#강릉 아파트만 출력하여 각 변수 평균 출력
+print('\n\n 강릉 아파트만 출력하여 각 변수 평균 출력')
+gangreng_csv = csv_read[gang_csv]
+print(gangreng_csv.mean())
+
+
+
+
---
+++ drinks.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+#파일 읽어오기
+file_drinks = pd.read_csv('./drinks.csv')
+
+#continent의 Nan 값 OT로 교체
+file_drinks.continent = file_drinks.continent.fillna('OT')
+
+#대륙별 beer_servings 컬럼 생성
+continent_beer_mean = file_drinks.groupby('continent').mean()['beer_servings'].to_frame()
+continent_beer_mean.rename(columns={'beer_servings':'beer_survings_cont_avg'},inplace=True)
+file_drinks = pd.merge(file_drinks,continent_beer_mean,how='inner',on='continent')
+print(file_drinks[['country','beer_servings','continent','beer_survings_cont_avg']].sample(5))
+print()
+
+#국가별 total_serveings 컬럼을 만들어서 병합하시오
+file_drinks['total_servings'] = file_drinks['beer_servings'] + file_drinks['spirit_servings'] + file_drinks['wine_servings']
+print(file_drinks[['country','beer_servings','spirit_servings','wine_servings','total_servings']])
+
+#전체 평균보다 적은 알코올을 섭취하는 대륙 중에서, sprit을 가장 많이 마시는 국가를 구하시오
+alcohol_mean = file_drinks['total_litres_of_pure_alcohol'].mean()
+continent_alcohol_mean = file_drinks.groupby('continent').mean()['total_litres_of_pure_alcohol'].to_frame()
+continent_alcohol_down = continent_alcohol_mean[continent_alcohol_mean.total_litres_of_pure_alcohol < alcohol_mean]
+continent_alcohol = pd.merge(file_drinks,continent_alcohol_down,how='inner',on='continent')
+country_spirit_best = continent_alcohol.sort_values(by='spirit_servings',ascending=False)
+print('전체 평균보다 적은 알코올을 섭취하는 대륙 중에서, sprit을 가장 많이 마시는 국가는 : ',country_spirit_best.iloc[0,0])
+
+#술 소비량 대비 알콜 비율 구하고 대한민국 순위를 구하시오.
+file_drinks['alcohol_percent'] = file_drinks['total_litres_of_pure_alcohol'] / file_drinks['total_servings']
+file_drinks['alcohol_percent']  = file_drinks['alcohol_percent'].fillna(0)
+file_drinks['alcohol_percent_rate'] = file_drinks['alcohol_percent'].rank(ascending=False)
+file_drinks['alcohol_percent_rate'] = file_drinks.alcohol_percent_rate.fillna(0)
+file_drinks['alcohol_percent_rate'] = file_drinks['alcohol_percent_rate'].apply(int)
+korea = file_drinks[file_drinks.country == 'South Korea']
+print(file_drinks[['country','alcohol_percent','alcohol_percent_rate']])
+print('술 소비량 대비 알콜 비율 구하고 대한민국 순위 :',korea.iloc[0,-1])
+print()
+
+#대륙별 술 소비량 대비 알콜 비율을 구하시오
+alcohol_cont_rate = file_drinks.groupby('continent').total_litres_of_pure_alcohol.sum() / file_drinks.groupby('continent').total_servings.sum()
+alcohol_cont_rate = alcohol_cont_rate.to_frame()
+alcohol_cont_rate.rename(columns={0:'alcohol_rate_continent'},inplace=True)
+alcohol_continent = pd.merge(file_drinks,alcohol_cont_rate,how='inner',on='continent')
+print(alcohol_continent[['country','continent','alcohol_rate_continent']].sample(5))
